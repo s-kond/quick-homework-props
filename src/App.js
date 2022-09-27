8832458ee71cfe24b7e09e8ed199944f25818f45
Stefan Kondring <kondring.s@gmx.de>
2022-09-27 18:34:22 +0200

    map person-cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { persons } from "./assets/persons";
 import './App.css';
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-  
+      <h1>Rick and Morty Characters</h1>
+      {persons.map(person => {
+        return <Card name={person.name} status={person.status} image={person.image} />
+      })}
     </div>
   );
 }
