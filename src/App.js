5b2c511113660d5ad33f1758320bc93540f7e101
Stefan Kondring <kondring.s@gmx.de>
2022-09-29 21:45:15 +0200

    update with useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import { persons } from "./assets/persons";
+/* import { persons } from "./assets/persons"; */
 import './App.css';
 import Card from "./components/Card";
+import { useEffect, useState } from "react";
+
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState("");
+
+  const apiURL = `https://rickandmortyapi.com/api/character${selected === "" ? "" : "/?status=" + selected}`
+
+  useEffect(() => {
+    console.log(apiURL);
+    fetch(apiURL)
+      .then(response => response.json())
+      .then(data => {
+        setData(data.results);
+      })
+  }, [selected])
+
+  function handleChange(event) {
+    console.log(event.target.value)
+    setSelected(event.target.value)
+  }
+
   return (
     <div className="App">
       <h1>Rick and Morty Characters</h1>
-      {persons.map(person => {
-        return <Card name={person.name} status={person.status} image={person.image} />
+      <select onChange={handleChange}>
+        <option value="">choose status</option>
+        <option value="Alive" >alive</option>
+        <option value="Dead">dead</option>
+        <option value="unknown">unknown</option>
+      </select>
+      {data.map(person => {
+        return <Card id={person.id} name={person.name} status={person.status} image={person.image} />
       })}
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import "./Card.css"
 
 function Card({name, status, image}) {
     return (
-        <article className={status === "Alive" ? "card alive" : status === "card Dead" ? "dead" : "card unknown"}>
+        <article className={status === "Alive" ? "card alive" : status === "Dead" ? "card dead" : "card unknown"}>
             <img src={image} alt={name} />
             <h2>{name}</h2>
         </article>
